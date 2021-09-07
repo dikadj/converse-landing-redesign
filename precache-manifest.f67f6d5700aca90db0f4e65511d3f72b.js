d685111dc529b3c28a4d63b6b2c3cbb16c6cf907
Primadika Dwijantara <primadika.d@mail.ugm.ac.id>
2021-09-08 01:19:34 +0700

    Updates

---
+++ precache-manifest.f67f6d5700aca90db0f4e65511d3f72b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "60b869497615d991b1d6",
+    "url": "/converse-landing-redesign/static/css/main.5747ed5a.chunk.css"
+  },
+  {
+    "revision": "60b869497615d991b1d6",
+    "url": "/converse-landing-redesign/static/js/main.4a922f2f.chunk.js"
+  },
+  {
+    "revision": "3c67f7774cbd3c92170a",
+    "url": "/converse-landing-redesign/static/js/runtime~main.64f00ba1.js"
+  },
+  {
+    "revision": "d6db0e508210156b2078",
+    "url": "/converse-landing-redesign/static/js/2.7df63cef.chunk.js"
+  },
+  {
+    "revision": "bed69641b587f6264359d1154cbd844c",
+    "url": "/converse-landing-redesign/static/media/highlight1.bed69641.jpg"
+  },
+  {
+    "revision": "d3c22187d39ebe20ff69e799910f2e70",
+    "url": "/converse-landing-redesign/static/media/highlight.d3c22187.jpg"
+  },
+  {
+    "revision": "7db1ed58d5229caef492fcc3d76ecef7",
+    "url": "/converse-landing-redesign/static/media/highlight2.7db1ed58.gif"
+  },
+  {
+    "revision": "3daf188da18b595e9b044b53d72e643c",
+    "url": "/converse-landing-redesign/static/media/highlight3.3daf188d.jpg"
+  },
+  {
+    "revision": "dfd73c73194a172cc75074ce97f66c3d",
+    "url": "/converse-landing-redesign/static/media/highlight4.dfd73c73.gif"
+  },
+  {
+    "revision": "9f43e530f8f325edb1cc0c7fa1b8b6fa",
+    "url": "/converse-landing-redesign/static/media/highlight4-md.9f43e530.gif"
+  },
+  {
+    "revision": "82ea6b67f4a18f8f548b261593aa8a17",
+    "url": "/converse-landing-redesign/static/media/highlight5.82ea6b67.jpg"
+  },
+  {
+    "revision": "98d6e0cdc621e736592d937a237db1b4",
+    "url": "/converse-landing-redesign/static/media/highlight5-md.98d6e0cd.jpg"
+  },
+  {
+    "revision": "f8b6f41741fc854e8fbbbff97c957fc7",
+    "url": "/converse-landing-redesign/static/media/highlight6-md.f8b6f417.jpg"
+  },
+  {
+    "revision": "2c3082f09c6630eeac8978dfa32f1a03",
+    "url": "/converse-landing-redesign/static/media/highlight6.2c3082f0.jpg"
+  },
+  {
+    "revision": "3a3b4729ba5920159301f9d6315838b1",
+    "url": "/converse-landing-redesign/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55b8eb0b77d017fa6379ef13555ec6d5.js"
+  "/converse-landing-redesign/precache-manifest.f67f6d5700aca90db0f4e65511d3f72b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/converse-landing-redesign/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
